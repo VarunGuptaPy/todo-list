b650f284cd458ed232e5c7f6275942fce31d2e9c
Varun <varun.gupta.py@gmail.com>
2022-04-26 17:42:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { name } = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser:true})
+const itemsSchema = {
+  name:String
+}
+const Item = mongoose.model("Item",itemsSchema)
+const Item1 = new Item({
+  name:"Welcome to your to do list"
+})
+const Item2 = new Item({
+  name:"Hit the + button to aff a new item."
+})
+const Item1 = new Item({
+  name:"Welcome to your to do list",
+})
+app.get("/", function(req, res) {
+  res.render("list", {listTitle: "Today", newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(80, function() {
+  console.log("Server started on port 80");
+});
